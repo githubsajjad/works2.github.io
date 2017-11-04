30230f54a69799a5b501f17c12ef1c4c6b99ab71
sajjad <md.sajjadali23@gmail.com>
2017-11-04 22:18:31 +0600

    class-four

---
+++ Class Four/assets/js/main.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function($){
+	$(".team-carousel").owlCarousel({
+		items: 3,
+		margin: 30,
+		loop: true,		
+		nav: true,
+		navText: ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],		
+	});
+
+});
